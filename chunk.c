b2d0852b8b7abe61731978720325b88eb5e899f9
Mathew Bravo <mathewbravo94@gmail.com>
2025-01-12 17:27:30 -0500

    Adding writing for chunks

--- chunk.c
+++ chunk.c
@@ -5,3 +5,14 @@ void initChunk(Chunk* chunk){
     chunk->capacity = 0;
     chunk->code = NULL;
 }
+
+void writeChunk(Chunk* chunk, uint8_t byte){
+    if(chunk->capacity < chunk->count + 1){
+        int oldCap = chunk->capacity;
+        chunk->capacity = GROW_CAPACITY(oldCap);
+        chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCap, chunk->capacity);
+    }
+    
+    chunk->code[chunk->count] = byte;
+    chunk->code++;
+}
